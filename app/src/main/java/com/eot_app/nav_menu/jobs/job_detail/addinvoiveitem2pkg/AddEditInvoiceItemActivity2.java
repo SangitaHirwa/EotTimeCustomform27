354aff815d9505e5009a93f7b5950775e9e509e2
sangita <sangitahirwa05nov@gmail.com>
2023-07-07 12:27:58 +0530

    #Eye014584 - [Android] Implement thrid calculation in Field worker app.

--- app/src/main/java/com/eot_app/nav_menu/jobs/job_detail/addinvoiveitem2pkg/AddEditInvoiceItemActivity2.java
+++ app/src/main/java/com/eot_app/nav_menu/jobs/job_detail/addinvoiveitem2pkg/AddEditInvoiceItemActivity2.java
@@ -2282,6 +2282,21 @@ public class AddEditInvoiceItemActivity2 extends
             String tax_Amount = AppUtility.getRoundoff_amount(String.valueOf(taxAmount));
             taxamount_value_txt.setText(tax_Amount);
         }
+        else if (App_preference.getSharedprefInstance().getLoginRes().getTaxCalculationType().equals("2")) {
+            double totalPrice = qty * rate;
+            double itemTotal = totalPrice + ((totalPrice * total_tax) / 100);
+            double discount = 0;
+            if (getDisCalculationType.equals("0"))
+                discount = ((totalPrice * dis) / 100);
+            else if (getDisCalculationType.equals("1"))
+                discount = dis;
+
+            amount = itemTotal - discount;
+
+            taxAmount = ((total_tax * rate * qty) / 100);
+            String tax_Amount = AppUtility.getRoundoff_amount(String.valueOf(taxAmount));
+            taxamount_value_txt.setText(tax_Amount);
+        }
 
         String amountString = AppUtility.getRoundoff_amount(String.valueOf(amount));
         amount_value_txt.setText(amountString);
