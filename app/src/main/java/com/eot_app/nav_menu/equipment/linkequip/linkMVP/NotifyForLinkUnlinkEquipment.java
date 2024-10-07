97a50cfb1887a6c2ba8e712d460133105b11b066
SangitaHirwa <sangitahirwa05nov@gmail.com>
2024-10-07 12:31:11 +0530

    07oct

---
+++ app/src/main/java/com/eot_app/nav_menu/equipment/linkequip/linkMVP/NotifyForLinkUnlinkEquipment.java
@@ -0,0 +1,7 @@
+package com.eot_app.nav_menu.equipment.linkequip.linkMVP;
+
+import com.eot_app.nav_menu.jobs.job_detail.requested_item.requested_itemModel.AddUpdateRequestedModel;
+
+public interface NotifyForLinkUnlinkEquipment {
+    void updateReqEquipmentList(String api_name, String message);
+}
